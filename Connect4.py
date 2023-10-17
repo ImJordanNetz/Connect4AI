6f62c333ffe8ce8db539654f65759ebc2277224d
ImJordanNetz <jonetz@eastsideprep.org>
2023-10-16 23:03:52 -0700

    same commit as last

--- Connect4.py
+++ Connect4.py
@@ -1,52 +1,89 @@
 class Connect4Game:
-    
+
+            
     def __init__(self, mode=0, width=7, height=6, in_a_row=4):
-        self.mode = 0
         
-        self.width = width
-        self.height = height
-        self.in_a_row = in_a_row
+        if isinstance(mode, int):
+            
+            self.width = width
+            self.height = height
+            self.in_a_row = in_a_row
+                    
+            self.isWin = 0 #turns 1 if true, 2 if tie
+            
+            self.tops = []
+            self.tie_check = [] #array that if tops = tie_check, its a tie
+            
+            self.moves = []
+            for col in range(self.width):
+                self.tie_check.append(self.height)
+                self.tops.append(0)
+            
+            self.relevent_board = []
+            self.board = [] # board is board[col][row]
+            self.relevent_board = [] # create an empty list for relevancy board
+            for col in range(self.width):
+                board_arr = []
+                relevancy_arr = []
+                for item in range(self.height):
+                    board_arr.append(0)
+                    relevancy_arr.append(0)  # create a new list for relevancy board
+                self.board.append(board_arr)
+                self.relevent_board.append(relevancy_arr)
+
                 
-        self.isWin = 0 #turns 1 if true, 2 if tie
-        
-        self.tops = []
-        self.tie_check = [] #array that if tops = tie_check, its a tie
-        
-        self.moves = []
-        for col in range(self.width):
-            self.tie_check.append(self.height)
-            self.tops.append(0)
-        
-        self.relevent_board = []
-        self.board = [] # board is board[col][row]
-        for col in range(self.width):
-            arr = []
-            for item in range(self.height):
-                arr.append(0)
-            self.board.append(arr)
-            self.relevent_board.append(arr)
             
-        
-        self.turn = 0 #0 -> P1, 1 -> P2
+            self.turn = 0 #0 -> P1, 1 -> P2
+                
+                
             
-            
+            self.mode = mode
+        else:
+            from copy import deepcopy
+            self.mode = 0
+            self.width = mode.width
+            self.height = mode.height
+            self.in_a_row = mode.in_a_row
+                        
+            self.isWin = mode.isWin #turns 1 if true, 2 if tie
+                
+            self.tops = mode.tops.copy()
+            self.tie_check = mode.tie_check.copy() #array that if tops = tie_check, its a tie
+                
+            self.moves = mode.moves.copy()
+                
+            self.relevent_board = deepcopy(mode.relevent_board)
+            self.board = deepcopy(mode.board) # board is board[col][row]
+            self.turn = mode.turn #0 -> P1, 1 -> P2
         
-        self.mode = mode
     
     def set_relevent_boards(self):
-        for col in range(len(self.board)):
-            for row in range(len(self.board[col])):
-                if self.board[col][row] != 0:
-                    in_range = False
-                    for i in range(len(self.tops)):
-                        dist_in_height = abs((self.tops[i]-row))
-                        dist_in_width = abs(col-i)
-                        if max(dist_in_height, dist_in_width) < self.in_a_row:
-                            in_range = True
-                    if in_range == False:
-                        self.relevent_board[col][row] = 3
-                    else:
-                        self.relevent_board[col][row] = self.board[col][row]
+        if self.isWin != 0:
+            self.relevent_board[self.moves[-1]][self.tops[self.moves[-1]]-1] = ((self.turn + 1) % 2) +1
+        else:
+            for col in range(len(self.board)):
+                for row in range(len(self.board[col])):
+                    if self.board[col][row] != 0 and self.relevent_board[col][row] != 3:
+                        space_before_whites = []
+                        space_before_whites.append(self.relevency_in_dir(col, row, -1, 0))
+                        space_before_whites.append(self.relevency_in_dir(col, row, 1, 0))
+                        
+                        space_before_whites.append(self.relevency_in_dir(col, row, 0, -1))
+                        space_before_whites.append(self.relevency_in_dir(col, row, 0, 1))
+                        space_before_whites.append(self.relevency_in_dir(col, row, -1, -1))
+                        space_before_whites.append(self.relevency_in_dir(col, row, 1, 1))
+                        
+                        space_before_whites.append(self.relevency_in_dir(col, row, -1, 1))
+                        space_before_whites.append(self.relevency_in_dir(col, row, 1, -1))
+                        
+                        if min(space_before_whites) >= self.in_a_row - 1:
+                            
+                            self.relevent_board[col][row] = 3
+
+                        else:
+                            self.relevent_board[col][row]=self.board[col][row]
+                            
+
 
 
             #modes:
@@ -101,18 +138,14 @@ class Connect4Game:
         
         up_left = self.count_in_dir(col, row, player_moved, -1, 1)
         down_right = self.count_in_dir(col, row, player_moved, 1, -1)
-        
         if left + right >= self.in_a_row-1:
             self.isWin = 1
         if down + 0 >= self.in_a_row-1:
             self.isWin = 1
-        
         if down_left + up_right >= self.in_a_row-1:
             self.isWin = 1
-    
         if up_left + down_right >= self.in_a_row-1:
             self.isWin = 1
-        
         if self.generate_legal_moves() == [] and self.isWin != 1:
             self.isWin = 2
 
@@ -131,6 +164,22 @@ class Connect4Game:
         if self.board[new_col][new_row] == player+1:
             #print("counted" + str(len(self.moves)) + " player " + str(player+1))
             return 1 + self.count_in_dir(new_col, new_row, player, offset_col, offset_row)
+        else:
+            return 0
+        
+        
+    def relevency_in_dir(self, col, row, offset_col, offset_row):
+        #if square looked if off board
+        new_col = col + offset_col
+        new_row = row + offset_row
+        if new_col < 0 or new_col >= self.width:
+            return self.in_a_row
+        if new_row < 0 or new_row >= self.height:
+            return self.in_a_row        
+        
+        if self.board[new_col][new_row] != 0:
+            #print("counted" + str(len(self.moves)) + " player " + str(player+1))
+            return 1 + self.relevency_in_dir(new_col, new_row, offset_col, offset_row)
         else:
             return 0
         
@@ -191,8 +240,4 @@ class Connect4Game:
         display += "\n"
 
         return display
-
-        
     
-game = Connect4Game()
-game.playHumanGame()
